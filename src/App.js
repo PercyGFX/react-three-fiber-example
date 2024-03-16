232b47c6bdde0b240f83d8e56cb15582e597a840
PercyGFX <isurangabtk@gmail.com>
2024-03-16 21:41:10 +0530

    gltfjsx used

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React, { Suspense, useRef } from "react";
 import { Canvas, useLoader, useFrame } from "@react-three/fiber";
 import { Box, OrbitControls, Stars } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Model from "./Scene";
 
-function Model() {
-  const gltf = useLoader(GLTFLoader, "/scene.gltf");
-  const group = useRef();
+// function Model() {
+//   const gltf = useLoader(GLTFLoader, "/scene.gltf");
+//   const group = useRef();
 
-  useFrame(() => {
-    group.current.rotation.y += 0.01; // Adjust the rotation speed as needed
-  });
+//   useFrame(() => {
+//     group.current.rotation.y += 0.01; // Adjust the rotation speed as needed
+//   });
 
-  return (
-    <group ref={group}>
-      <primitive object={gltf.scene} />
-    </group>
-  );
-}
+//   return (
+//     <group ref={group}>
+//       <primitive object={gltf.scene} />
+//     </group>
+//   );
+// }
 
 function App() {
   return (
